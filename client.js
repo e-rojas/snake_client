be951bc7f7b6d15d77c6464ad08add5c43a12332
javascript01fan <ejrojas@icloud.com>
2019-08-28 13:03:26 -0400

    able to send a message to the game

--- client.js
+++ client.js
@@ -5,9 +5,15 @@ const connect = ()=>{
         host:'localhost',
         port:50541
     });
+    //event handler to receive data
     conn.on('data',data=>{
         console.log('incoming data:',data)
-    })
+    });
+    //event handler to check connection 
+    conn.on('connect',() =>{
+    console.log('Connection succesfully stablished');
+    conn.write('Name: EJR');
+    });
 
     conn.setEncoding('utf8');
     return conn
