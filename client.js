0ea285dcba68abfc3b2ef5532fd0f85ab8f95aa2
javascript01fan <ejrojas@icloud.com>
2019-08-28 14:44:34 -0400

    to automatically move up one square each time we connect

--- client.js
+++ client.js
@@ -12,7 +12,29 @@ const connect = ()=>{
     //event handler to check connection 
     conn.on('connect',() =>{
     console.log('Connection succesfully stablished');
-    conn.write('Name: EJR');
+    //conn.write('Name: EJR');
+    const messages = ['Move: up','Move: left','Move:left','Move: up','Move: right']
+    let number = 0
+    for (const message of messages) {
+        setTimeout(() => {
+            conn.write(message)
+        }, number += 500);
+    }
+
+    /*  setTimeout(() => {
+        conn.write('Move: up');
+     }, 50);
+     setTimeout(() => {
+        conn.write('Move: up');
+     }, 100);
+     setTimeout(() => {
+        conn.write('Move: left');
+     }, 150);
+     setTimeout(() => {
+        conn.write('Move: left');
+     }, 200); */
+     
+     
     });
 
     conn.setEncoding('utf8');
